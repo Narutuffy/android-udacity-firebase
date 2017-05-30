5e7ddfc51a1fc7e212d6d069a56d78d2743ca367
Vamsi <s.vamsirao@gmail.com>
2017-05-30 23:28:53 +0530

    Add FirebaseMessage Service to read data messages

--- app/src/main/java/android/example/com/squawker/SquawkFirebaseMessageService.java
+++ app/src/main/java/android/example/com/squawker/SquawkFirebaseMessageService.java
@@ -1,6 +1,16 @@
 package android.example.com.squawker;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
 import android.example.com.squawker.provider.SquawkContract;
+import android.example.com.squawker.provider.SquawkProvider;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -51,9 +61,72 @@ public class SquawkFirebaseMessageService extends FirebaseMessagingService {
             Log.d(LOG_TAG, "Message data payload: " + data);
 
             // Send a notification that you got a new message
-           // sendNotification(data);
-           // insertSquawk(data);
+            // Send a notification that you got a new message
+            sendNotification(data);
+            insertSquawk(data);
 
         }
+    }
+
+    /**
+     * Inserts a single squawk into the database;
+     *
+     * @param data Map which has the message data in it
+     */
+    private void insertSquawk(final Map<String, String> data) {
+
+        // Database operations should not be done on the main thread
+        AsyncTask<Void, Void, Void> insertSquawkTask = new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                ContentValues newMessage = new ContentValues();
+                newMessage.put(SquawkContract.COLUMN_AUTHOR, data.get(JSON_KEY_AUTHOR));
+                newMessage.put(SquawkContract.COLUMN_MESSAGE, data.get(JSON_KEY_MESSAGE).trim());
+                newMessage.put(SquawkContract.COLUMN_DATE, data.get(JSON_KEY_DATE));
+                newMessage.put(SquawkContract.COLUMN_AUTHOR_KEY, data.get(JSON_KEY_AUTHOR_KEY));
+                getContentResolver().insert(SquawkProvider.SquawkMessages.CONTENT_URI, newMessage);
+                return null;
+            }
+        };
+
+        insertSquawkTask.execute();
+    }
+
+
+    /**
+     * Create and show a simple notification containing the received FCM message
+     *
+     * @param data Map which has the message data in it
+     */
+    private void sendNotification(Map<String, String> data) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        // Create the pending intent to launch the activity
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String author = data.get(JSON_KEY_AUTHOR);
+        String message = data.get(JSON_KEY_MESSAGE);
+
+        // If the message is longer than the max number of characters we want in our
+        // notification, truncate it and add the unicode character for ellipsis
+        if (message.length() > 30) {
+            message = message.substring(0,30) + "\u2026";
+        }
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_duck)
+                .setContentTitle(String.format(getString(R.string.notification_message), author))
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 }
